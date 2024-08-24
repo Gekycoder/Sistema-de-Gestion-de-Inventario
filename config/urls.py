6c81f4f522403af9fbc88830f09a8e1b47c0befa
Geky . <nathansleon@gmail.com>
2024-08-24 12:36:29 -0400

    colocar la redireccion inicial del localhost al encender el server (api/auth)

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,18 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
+# Define la vista para redirigir la raíz a /api/auth/
+def redirect_to_auth(request):
+    return redirect('/api/auth/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('inventory_api.urls')), 
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', redirect_to_auth),
 ]
